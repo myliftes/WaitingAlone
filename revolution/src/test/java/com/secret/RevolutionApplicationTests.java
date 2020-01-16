77943073b81d54ec1183d86c113535cec72193e1
myliftes <1850746556@qq.com>
2020-01-16 20:50:12 +0800

    第一次提交

---
+++ revolution/src/test/java/com/secret/RevolutionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.secret;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RevolutionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
